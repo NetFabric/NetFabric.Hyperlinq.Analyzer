0e5164e3fd366f90aad1b6cf669084212702a168
Antao Almada <antao.almada@farfetch.com>
2023-10-11 17:26:48 +0100

    Add Dictionary to the benchmark

--- NetFabric.Hyperlinq.Analyzer.Benchmarks/HLQ010_UseForLoop.ValueTypeEnumerator.cs
+++ NetFabric.Hyperlinq.Analyzer.Benchmarks/HLQ010_UseForLoop.ValueTypeEnumerator.cs
@@ -1,10 +1,14 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 
 namespace NetFabric.Hyperlinq.Analyzer.Benchmarks;
 
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
 public class HLQ010_UseForLoop_ValueTypeEnumerator
 {
-    ArraySegment<int> source;
+    ArraySegment<int> arraySegment;
+    Dictionary<int, int> dictionary;
 
     [Params(100, 10_000)]
     public int Count { get; set; }
@@ -12,25 +16,53 @@ public class HLQ010_UseForLoop_ValueTypeEnumerator
     [GlobalSetup]
     public void GlobalSetup()
     {
-        source = new ArraySegment<int>(Enumerable.Range(0, Count).ToArray());
+        var array = Enumerable.Range(0, Count).ToArray();
+        arraySegment = new ArraySegment<int>(array);
+        dictionary = array.ToDictionary(item => item);
     }
 
+    [BenchmarkCategory("ArraySegment")]
     [Benchmark(Baseline = true)]
-    public int Foreach()
+    public int ArraySegment_Foreach()
     {
         var sum = 0;
-        foreach (var item in source!)
+        foreach (var item in arraySegment!)
             sum += item;
         return sum;
     }
 
+    [BenchmarkCategory("ArraySegment")]
     [Benchmark]
-    public int For()
+    public int ArraySegment_For()
     {
         var sum = 0;
-        for (var index = 0; index < source!.Count; index++)
+        for (var index = 0; index < arraySegment!.Count; index++)
         {
-            var item = source![index];
+            var item = arraySegment![index];
+            sum += item;
+        }
+        return sum;
+    }
+
+
+    [BenchmarkCategory("Dictionary")]
+    [Benchmark(Baseline = true)]
+    public int Dictionary_Foreach()
+    {
+        var sum = 0;
+        foreach (var item in dictionary!)
+            sum += item.Value;
+        return sum;
+    }
+
+    [BenchmarkCategory("Dictionary")]
+    [Benchmark]
+    public int Dictionary_For()
+    {
+        var sum = 0;
+        for (var index = 0; index < dictionary!.Count; index++)
+        {
+            var item = dictionary![index];
             sum += item;
         }
         return sum;
